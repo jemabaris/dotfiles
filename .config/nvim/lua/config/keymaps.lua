7db967094152c929ebdb0eea5fab1865cdcdd176
jemabaris <janis.robens@gmail.com>
2026-03-27 04:47:32 +0100

    Added more keybinds to switch to previous/next workspace

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,22 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
 vim.keymap.set("i", "jk", "<ESC>", { noremap = false })
 vim.keymap.set("n", "<leader>;", Snacks.dashboard.open, { desc = "Open Snacks Dashboard" })
 -- vim.g.maplocalleader = ","
---
--- Janis Custom Setup to quickly run current Python file in a terminal
+
+-- # Janis Custom Setup to quickly run current Python file in a terminal
 vim.keymap.set("n", "<localleader>rr", function()
   vim.cmd("write")
   vim.cmd("belowright 20split")
   vim.cmd("terminal python " .. vim.fn.expand("%"))
 end, { desc = "Run Python File" })
--- Clear copilot suggestion with Esc if visible, otherwise preserve default Esc behavior
+
+-- # Clear copilot suggestion with Esc if visible, otherwise preserve default Esc behavior
 -- vim.keymap.set("n", "<esc>", function()
 --     if not require("copilot-lsp.nes").clear() then
 --         -- fallback to other functionality
 --     end
 -- end, { desc = "Clear Copilot suggestion or fallback" })
+
+-- # Jump to middle of current line JR
 vim.keymap.set("n", "gm", function()
   local line = vim.api.nvim_get_current_line()
   local col = math.floor(#line / 2)
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,25 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.scrolloff = 8
-vim.opt.clipboard = "unnamedplus"
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+-- Sync clipboard between OS and Neovim.
+vim.schedule(function()
+  vim.o.clipboard = "unnamedplus"
+end)
+
+-- -- Diagnostic Config & Keymaps
+-- vim.diagnostic.config({
+--   update_in_insert = false,
+--   severity_sort = true,
+--   float = { border = "rounded", source = "if_many" },
+--   underline = { severity = { min = vim.diagnostic.severity.WARN } },
+--
+--   -- Can switch between these as you prefer
+--   virtual_text = true, -- Text shows up at the end of the line
+--   virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+--
+--   -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+--   jump = { float = true },
+-- })
