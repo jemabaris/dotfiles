b2afa8e0d634d20f4d2e7f8df87eec51476fc9a7
jemabaris <janis.robens@gmail.com>
2026-04-12 16:13:15 +0200

    Added gtk-3.0 and gtk-4.0

--- .config/waybar/vpn-toggle.sh
+++ .config/waybar/vpn-toggle.sh
@@ -4,6 +4,5 @@ if [ -d /proc/sys/net/ipv4/conf/proton0 ]; then
   CONNECTION=$(nmcli -t -f NAME,DEVICE connection show --active | grep proton0 | cut -d: -f1)
   nmcli connection down "$CONNECTION"
 else
-  CONNECTION=$(nmcli -t -f NAME,TYPE connection show | grep wireguard | tail -1 | cut -d: -f1)
-  nmcli connection up "$CONNECTION"
+  protonvpn-app &
 fi
