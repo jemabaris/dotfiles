da7615672370c4f9e164295c992e42e4472ce74c
jemabaris <janis.robens@gmail.com>
2026-04-13 23:28:38 +0200

    Disable diagnostics and spelling in markdown files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable diagnostics and formatting for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function(ev)
+    vim.diagnostic.enable(false, { bufnr = ev.buf })
+    vim.opt_local.spell = false
+  end,
+})
