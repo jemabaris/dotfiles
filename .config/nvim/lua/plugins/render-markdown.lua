52585d3c815e9b0238e3ba59255aa57e8f70e330
jemabaris <janis.robens@gmail.com>
2026-04-14 02:00:00 +0200

    Updated custom github waybar notification

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -2,9 +2,32 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
-      render_modes = { "n", "c", "t" }, -- normal mode at minimum
+      render_modes = { "n", "c", "t" },
+
       image = {
         backend = "snacks",
+      },
+
+      custom_handlers = {
+        image = function(bufnr, _, text)
+          local api = require("obsidian.api")
+
+          -- Obsidian style: ![[image.png]]
+          local obsidian_match = text:match("^!%[%[(.-)%]%]$")
+          if obsidian_match then
+            return {
+              src = api.resolve_attachment_path(obsidian_match),
+            }
+          end
+
+          -- Standard markdown: ![alt](path)
+          local md_match = text:match("^!%[.-%]%((.-)%)$")
+          if md_match then
+            return {
+              src = api.resolve_attachment_path(md_match),
+            }
+          end
+        end,
       },
     },
   },
