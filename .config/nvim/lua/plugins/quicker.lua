a930c956903d541ee3f9a680f770faac0fa08fd1
jemabaris <janis.robens@gmail.com>
2026-05-16 16:19:06 +0200

    Basic config of quicker.nvim

--- .config/nvim/lua/plugins/quicker.lua
+++ .config/nvim/lua/plugins/quicker.lua
@@ -5,5 +5,35 @@ return {
     ---@module "quicker"
     ---@type quicker.SetupOptions
     opts = {},
+    keys = {
+      {
+        "<leader>xp",
+        function()
+          require("quicker").toggle()
+        end,
+        desc = "Quickfix (quicker)",
+      },
+      {
+        "<leader>xk",
+        function()
+          require("quicker").toggle({ loclist = true })
+        end,
+        desc = "Loclist (quicker)",
+      },
+      {
+        ">",
+        function()
+          require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+        end,
+        desc = "Expand quickfix context",
+      },
+      {
+        "<",
+        function()
+          require("quicker").collapse()
+        end,
+        desc = "Collapse quickfix context",
+      },
+    },
   },
 }
